57933a2e3797ff051e78e042d8b092bb42188e50
colmeta <nsubugacollin@gmail.com>
2025-10-20 06:49:05 +0300

    Update __init__.py

--- integrations/__init__.py
+++ integrations/__init__.py
@@ -1,1 +1,15 @@
+"""
+Integration modules for chatbot
+Import these in your main chatbot_app.py file
+"""
 
+from .whatsapp_integration import WhatsAppIntegration, process_incoming_whatsapp_message
+from .crm_integration import CRMIntegration
+from .calendar_integration import CalendarIntegration
+
+__all__ = [
+    'WhatsAppIntegration',
+    'process_incoming_whatsapp_message',
+    'CRMIntegration',
+    'CalendarIntegration'
+]
